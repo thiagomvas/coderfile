757acfaacd043f64de6eda4286f17d104a8efbbf
Thiago <thiagomvas@gmail.com>
2024-01-28 12:11:44 -0300

    Add templates open command

---
+++ coderfile.CLI/Commands/Templates.cs
@@ -0,0 +1,18 @@
+﻿using Cocona;
+using coderfile.Core.IO;
+using System.Diagnostics;
+
+namespace coderfile.CLI.Commands
+{
+	public class Templates
+	{
+		[Command("open")]
+		public void OpenTemplateFolder()
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("Opening template folder...");
+			Console.ResetColor();
+			Process.Start("explorer.exe", TemplateGenerator.templateFolder);
+		}
+	}
+}
