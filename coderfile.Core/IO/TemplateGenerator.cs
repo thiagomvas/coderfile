6c4daa2fc617e742773b4b6d8279caa309ad53f1
Thiago <thiagomvas@gmail.com>
2024-01-28 12:05:01 -0300

    Update template file generation

---
+++ coderfile.Core/IO/TemplateGenerator.cs
@@ -0,0 +1,64 @@
+﻿using System.Reflection;
+
+namespace coderfile.Core.IO
+{
+	public class TemplateGenerator
+	{
+		public const string rootFromDll = @"..\..\..\..\contentFiles\any\any\Templates";
+
+		/// <summary>
+		/// Shorthand for calling both <see cref="FindTemplateFile(string, string)"/> and <see cref="CloneTemplateFile(string, string)"/>
+		/// </summary>
+		/// <param name="filename"></param>
+		/// <param name="folderPath"></param>
+		/// <param name="template"></param>
+		public static void Generate(string filename, string folderPath, string template)
+		{
+			string? path = FindTemplateFile(folderPath, template);
+
+			if (path == null)
+			{
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.Write("❗ ");
+				Console.ResetColor();
+				Console.WriteLine($"{filename}: Failed to find template file for {template}");
+			}
+			else
+				CloneTemplateFile(filename, path);
+		}
+
+		public static string? FindTemplateFile(string folderPath, string partialFileName)
+		{
+			string path = Path.Combine(Assembly.GetExecutingAssembly().Location, rootFromDll, folderPath);
+
+			string[] allFiles = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+
+			string? matchingFile = allFiles.FirstOrDefault(file =>
+				Path.GetFileNameWithoutExtension(file)
+				.StartsWith(partialFileName, StringComparison.OrdinalIgnoreCase));
+
+			return matchingFile;
+		}
+
+		public static void CloneTemplateFile(string name, string templatePath)
+		{
+			string content = File.ReadAllText(templatePath);
+			string path = Path.Combine(Directory.GetCurrentDirectory(), name);
+
+			if (FileManager.CreateFile(path, content))
+			{
+				Console.ForegroundColor = ConsoleColor.Green;
+				Console.Write("✓ ");
+				Console.ResetColor();
+				Console.WriteLine($"{name}: Created file at {path}");
+			}
+			else
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.Write("X ");
+				Console.ResetColor();
+				Console.WriteLine($"{name}: Failed to create file at {path}");
+			}
+		}
+	}
+}
